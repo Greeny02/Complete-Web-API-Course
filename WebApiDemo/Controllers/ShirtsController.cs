014935e05b5804776c16cc8d216830a267fbe7db
Nabeel Crichton <nabeel.crichton@redpandasoftware.co.za>
2024-08-22 08:43:30 +0200

    First commit

---
+++ WebApiDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.Filters;
+using WebApiDemo.Models;
+using WebApiDemo.Models.Repositories;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController : ControllerBase
+    {
+
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+
+        [HttpGet("{id}")]
+        [Shirt_ValidateShirtIdFilter]
+        public IActionResult GetShirtByID(int id)
+        {
+            return Ok(ShirtRepository.GetShirtByID(id));
+        }
+
+        [HttpPost]
+        public string CreateShirt([FromBody]Shirt shirt)
+        {
+            return "Creating a shirt";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Updating shirt with ID: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id) 
+        {
+            return $"Deleting shirt with ID: {id}";        
+        }
+    }
+}
---
+++ WebApiDemo/Models/Repositories/ShirtRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApiDemo.Models.Repositories
+{
+    public static class ShirtRepository
+    {
+        private static List<Shirt> shirts = new List<Shirt>()
+        {
+            new Shirt{ShirtId = 1, Brand="My Brand", Colour="Blue", Gender="Male", Price=30, Size=10},
+            new Shirt{ShirtId = 2, Brand="My Brand", Colour="Black", Gender="Male", Price=35, Size=12},
+            new Shirt{ShirtId = 3, Brand="Your Brand", Colour="Pink", Gender="Female", Price=28, Size=8},
+            new Shirt{ShirtId = 4, Brand="Your Brand", Colour="Yellow", Gender="Female", Price=30, Size=9},
+        };
+
+        public static bool ShirtExists(int id)
+        {
+            return shirts.Any(x => x.ShirtId == id);
+        }
+
+        public static Shirt? GetShirtByID(int id)
+        {
+            return shirts.FirstOrDefault(x => x.ShirtId == id);
+        }
+    }
+}
